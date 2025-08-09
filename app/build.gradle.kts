54293766a646e1ad51af56b916bf798fab72541f
mahmoudragap77 <121394672+mahmoudragap77@users.noreply.github.com>
2025-08-09 08:37:14 +0400

    Initial  a new e-commerce app

---
+++ app/build.gradle.kts
@@ -0,0 +1,74 @@
+plugins {
+    alias(libs.plugins.android.application)
+    alias(libs.plugins.kotlin.android)
+    id ("kotlin-parcelize")
+    id ("androidx.navigation.safeargs.kotlin")
+    id ("dagger.hilt.android.plugin")
+    id ("kotlin-kapt")
+}
+
+android {
+    namespace = "com.example.klieneapp"
+    compileSdk = 36
+
+    defaultConfig {
+        applicationId = "com.example.klieneapp"
+        minSdk = 21
+        targetSdk = 36
+        versionCode = 1
+        versionName = "1.0"
+
+        testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
+    }
+
+    buildTypes {
+        release {
+            isMinifyEnabled = false
+            proguardFiles(
+                getDefaultProguardFile("proguard-android-optimize.txt"),
+                "proguard-rules.pro"
+            )
+        }
+    }
+    compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_11
+        targetCompatibility = JavaVersion.VERSION_11
+    }
+    kotlinOptions {
+        jvmTarget = "11"
+    }
+}
+
+dependencies {
+
+    implementation(libs.androidx.core.ktx)
+    implementation(libs.androidx.appcompat)
+    implementation(libs.material)
+    implementation(libs.androidx.activity)
+    implementation(libs.androidx.constraintlayout)
+    testImplementation(libs.junit)
+    androidTestImplementation(libs.androidx.junit)
+    androidTestImplementation(libs.androidx.espresso.core)
+
+
+
+    //loading button
+
+    //Glide
+    implementation ("com.github.bumptech.glide:glide:4.16.0")
+
+    //circular image
+    implementation ("de.hdodenhof:circleimageview:3.1.0")
+
+    //viewpager2 indicatior
+    implementation ("androidx.viewpager2:viewpager2:1.1.0")
+
+    //stepView
+    implementation("com.github.shuhart:stepview:1.5.1")
+    //Android Ktx
+    implementation ("androidx.navigation:navigation-fragment-ktx:2.9.3")
+
+    //Dagger hilt
+    implementation ("com.google.dagger:hilt-android:2.38.1")
+     kapt ("com.google.dagger:hilt-compiler:2.38.1")
+}
---
+++ build.gradle.kts
@@ -0,0 +1,15 @@
+
+buildscript {
+    dependencies {
+        classpath ("com.google.dagger:hilt-android-gradle-plugin:2.40.1")
+        classpath ("com.google.gms:google-services:4.3.13")
+
+        val nav_version = "2.5.0"
+        classpath ("androidx.navigation:navigation-safe-args-gradle-plugin:$nav_version")
+    }
+}
+
+plugins {
+    alias(libs.plugins.android.application) apply false
+    alias(libs.plugins.kotlin.android) apply false
+}
