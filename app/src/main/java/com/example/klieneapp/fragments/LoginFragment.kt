4bf5e4254172ea5991def84bb35302c4f4edaa2a
mahmoudragap77 <121394672+mahmoudragap77@users.noreply.github.com>
2025-08-10 08:31:08 +0400

    Design the introduction fragments

---
+++ app/src/main/java/com/example/klieneapp/fragments/LoginFragment.kt
@@ -0,0 +1,12 @@
+package com.example.klieneapp.fragments
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.example.klieneapp.R
+
+class LoginFragment : Fragment(R.layout.fragment_login) {
+
+}
