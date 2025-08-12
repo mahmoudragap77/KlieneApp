f10baf18d893a7f3492e984a33cf1f4ea0ad719e
mahmoudragap77 <121394672+mahmoudragap77@users.noreply.github.com>
2025-08-12 08:29:35 +0400

    WIP: Implement RegisterViewModel with FirebaseAuth

--- build.gradle.kts
+++ build.gradle.kts
@@ -2,7 +2,6 @@
 buildscript {
     dependencies {
         classpath ("com.google.dagger:hilt-android-gradle-plugin:2.40.1")
-        classpath ("com.google.gms:google-services:4.3.13")
 
         val nav_version = "2.5.0"
         classpath ("androidx.navigation:navigation-safe-args-gradle-plugin:$nav_version")
@@ -12,4 +11,5 @@ buildscript {
 plugins {
     alias(libs.plugins.android.application) apply false
     alias(libs.plugins.kotlin.android) apply false
+    alias(libs.plugins.google.gms.google.services) apply false
 }
